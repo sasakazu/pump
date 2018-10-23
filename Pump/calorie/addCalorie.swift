8e3645ba018466dccde726e2d833652be32fcedb
sasakazu <sasakura.company@gmail.com>
2018-10-23 19:32:00 +0900

    file add..

---
+++ Pump/calorie/addCalorie.swift
@@ -0,0 +1,27 @@
+//
+//  addCalorie.swift
+//  Pump
+//
+//  Created by 笹倉 一也 on 2018/10/23.
+//  Copyright © 2018年 sasakura.company. All rights reserved.
+//
+
+import UIKit
+
+class addCalorie: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    
+    @IBAction func cancelBtn(_ sender: Any) {
+        
+        dismiss(animated: true, completion: nil)
+    }
+    
+    
+
+}
---
+++ Pump/calorie/calorie.swift
@@ -0,0 +1,30 @@
+//
+//  calorie.swift
+//  Pump
+//
+//  Created by 笹倉 一也 on 2018/10/23.
+//  Copyright © 2018年 sasakura.company. All rights reserved.
+//
+
+import UIKit
+
+class calorie: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
